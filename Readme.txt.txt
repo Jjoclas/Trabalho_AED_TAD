78647f3f1a47248b00dbcb5937809f3dfa3bcb53
Jose Claudio <jjoclas@hotmail.com>
2020-07-06 13:45:57 -0300

    adicionado executaveis e readme.txt

---
+++ Readme.txt.txt
@@ -0,0 +1,5 @@
+Para executar o o programa abra seu prompt de comando na pasta 'python' e digite o codigo:' pyhton main.py '.
+Certifique-se de ter Python instalado. 
+o programa foi testado em python 3.7.2
+
+ps: Há dois executaveis, porem ambos não produziram resultados confiaveis em outros sistemas, ambos se encontram na pasta pyhton/exe
---
+++ python/setup.py
@@ -0,0 +1,21 @@
+#arquivo utilizado para criar um exe
+from cx_Freeze import setup, Executable
+
+base = None    
+
+executables = [Executable("main.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "<any name>",
+    options = options,
+    version = "<any number>",
+    description = '<any description>',
+    executables = executables
+)
